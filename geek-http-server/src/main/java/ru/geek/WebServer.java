d214ca8f92c685918202d1f7d6499f3ed746e52a
Bogdan2505 <75305511+Bogdan2505@users.noreply.github.com>
2022-10-02 17:18:26 +0300

    first commit

---
+++ geek-http-server/src/main/java/ru/geek/WebServer.java
@@ -0,0 +1,35 @@
+package ru.geek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class WebServer {
+
+
+
+    public static void main(String[] args) {
+
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            System.out.println("Server started!");
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected!");
+                new Thread(() -> new HandleRequest(socket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
